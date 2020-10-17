47ea1546bab00c75996abcfeae73926aa5947a24
franciscobeccaria <fbeccaria24@gmail.com>
2020-10-17 00:40:33 -0300

    Agregar sección y Borrar sección terminados preliminares

--- script.js
+++ script.js
@@ -1,29 +1,55 @@
-// Agregar sección
+// Próximas funciones
+// Borrar una sección. 
+// Agregar jugador. 
+// Borrar jugador.
+// Que se pueda agregar una sección encima o debajo. 
+// Que blank-player no se pueda borrar, asi nunca va haber una sección vacia. 
+// Crear página que se muestre cuando hay 0 secciones. 
+// Resolver el espacio vacio entre secciones y footer. 
+//// Probablemente lo mejor es que haya un fondo piola. 
+// Crear footer. 
+// Mover jugadores: cambiar orden dentro de la sección. Y mover a otra sección. 
+// Otra cosa que me faltó, es que no se pueda repetir el nombre de las secciones, no puede haber dos arqueros y arqueros.
 
-const addSection = document.getElementById('add-section')
-const addSectionToMobile = document.getElementById('add-section-to-mobile')
+//IMPORTANTES
+// Agregar que cuando posiciones el mouse encima de los buttons-to-mobile, apareza un mensajito de lo que hace. 
 
+const body = document.getElementById('body')
 const main = document.getElementById('main')
 
-// PROXIMA TAREA: CAMBIAR EL inner html a textContent y etc. Así podemos usar un id creado por nosotros. Porque por innerHTML se ve que no se puede. 
+const arrayOfSections = ['arqueros', ]
 
-addSection.addEventListener('click', () => {
+// Agregar sección
+
+// Problema: Terminé utilizando onclick en html, en vez de un addEventListener u otro en js. No encontré ninguna forma de realizar el proceso mediante js. getElementByClassName no funcionó. Y si funcionaba, terminaba no funcionando en los nuevos. 
+
+// Agregar modal. En el que se preguntará, Agregar encima o agregar debajo de que section? Si no hay ninguna section se hace el appendChild de siempre. 
+// El modal puede que se haga con condicionales. 
+
+const addSectionClick = () => {
     console.log('Hola mundo')
 
-    let sectionTitlePositions = prompt("Titulo de la Sección (Ejemplo: Arqueros)").toUpperCase();
+    let sectionTitlePositions = prompt("Titulo de la Sección (Ejemplo: Arqueros)").toUpperCase()
+
+    let sectionTitlePositionsDelete = sectionTitlePositions.toLowerCase().replace(/[" "]/gi, "-").concat("-delete")
+    let sectionTitlePositionsDeleteToMobile = sectionTitlePositions.toLowerCase().replace(/[" "]/gi, "-").concat("-delete-to-mobile")
 
     const playerDescription =`
     <section class="section-title">
     <div class="add-buttons">
-        <button class="add-section" id="add-section">Agregar sección</button>
+        <button class="add-section" id="add-section" onclick="addSectionClick()">Agregar sección</button>
         <button class="add-player">Agregar jugador</button>
+        <button class="delete-section" id="${sectionTitlePositionsDelete}" onclick="deleteSectionClick()">Borrar sección</button>
     </div>
     <div class="add-buttons-to-mobile">
         <div class="add-section-to-mobile">
-            <a class="add-section-icon-container" id="add-section-to-mobile" href="#"><img class="add-section-icon" src="icons/add.png" alt=""></a>
+            <a class="add-section-icon-container" id="add-section-to-mobile" href="#" onclick="addSectionClick()"><img class="add-section-icon" src="icons/add.png" alt=""></a>
         </div>
         <div class="add-player-to-mobile">
             <a class="add-player-icon-container" href="#"><img class="add-player-icon" src="icons/add-friend.png" alt=""></a>
+        </div>
+        <div class="delete-section-to-mobile">
+            <a class="delete-section-icon-container" id="${sectionTitlePositionsDeleteToMobile}" href="" onclick="deleteSectionClick()"><img class="delete-section-icon" src="icons/add-friend.png" alt=""></a>
         </div>
     </div>
     <h3 class="section-title-positions">${sectionTitlePositions}</h3>
@@ -35,11 +61,6 @@ addSection.addEventListener('click', () => {
     </div>
     </section>
     `
-
-    const sectionTitle = document.createElement('section')
-    sectionTitle.classList.add('section-title')
-    document.body.appendChild
-
 
     const playerDescriptionTwo = `
     <section class="section-players">
@@ -75,85 +96,31 @@ addSection.addEventListener('click', () => {
             </div>
         </div>`
 
-
     const newElement = document.createElement('section')
-    const newElementTwo = document.createElement('section')
+    const newElementID = newElement.id = sectionTitlePositions.toLowerCase().replace(/[" "]/gi, "-")
+    newElement.classList.add('example')
+
     main.appendChild(newElement)
-    newElement.innerHTML = playerDescription
-    main.appendChild(newElementTwo)
-    newElementTwo.innerHTML = playerDescriptionTwo
-})
+    newElement.innerHTML = playerDescription + playerDescriptionTwo
 
-addSectionToMobile.addEventListener('click', () => {
-    console.log('Hola mundo')
+    arrayOfSections.push(newElementID)
+    console.log(arrayOfSections)
+}
 
-    let sectionTitlePositions = prompt("Titulo de la Sección (Ejemplo: Arqueros)").toUpperCase();
+// Borrar sección
 
-    const playerDescription =`
-    <section class="section-title">
-    <div class="add-buttons">
-        <button class="add-section" id="add-section">Agregar sección</button>
-        <button class="add-player">Agregar jugador</button>
-    </div>
-    <div class="add-buttons-to-mobile">
-        <div class="add-section-to-mobile">
-            <a class="add-section-icon-container" id="add-section-to-mobile" href="#"><img class="add-section-icon" src="icons/add.png" alt=""></a>
-        </div>
-        <div class="add-player-to-mobile">
-            <a class="add-player-icon-container" href="#"><img class="add-player-icon" src="icons/add-friend.png" alt=""></a>
-        </div>
-    </div>
-    <h3 class="section-title-positions">${sectionTitlePositions}</h3>
-    <div class="players-info">
-        <div class="on-loan">
-            <div class="on-loan-icon-container"><img class="on-loan-icon" src="icons/starred.png" alt=""></div>
-            <p class="on-loan-p">Prestado</p>
-        </div>
-    </div>
-    `
+// Problema: Se debería poder elegir que sección eliminar dando en el boton correspondiente de cada sección. 
+// La otra opcion es que al usuario le aparezca un modal y ahi elija que sección eliminar, pero sin escribir. 
 
-    const playerDescriptionTwo = `</section>
-    <section class="section-players">
-    <div class="player-card">
-            <div class="player-card-description">
-                <div class="league">
-                    <img class="league-photo" src="img/leagues/total-blank.png" alt="">
-                </div>
-                <div class="club">
-                    <img class="club-photo" src="img/clubs/total-blank.png" alt="">
-                </div>
-                <div class="main-position">
-                    <p class="main-position-description"><br></p>
-                </div>
-                <div class="valored">
-                    <p class="valored-description"></p>
-                </div>
-                <div class="age">
-                    <p class="age-description"></p>
-                </div>
-                <div class="born-year">
-                    <p class="born-year-description"></p>
-                </div>
-                <div class="player-photo-container">
-                    <img class="player-photo" src="img/players/blank.png" alt="">
-                </div>
-                <div class="player-on-loan">
-                    <img class="player-on-loan-img" src="icons/total-blank.png" alt="">
-                </div>
-            </div>
-            <div class="player-card-name">
-                <a class="player-card-name-link" href="#"><p class="name"><span class="lastname">AGREGAR JUGADOR</span></p></a>
-            </div>
-        </div>`
+// La opción con modal y usar condicionales if es la mejor para mi. 
 
-    const main = document.getElementById('main')
-    const newElement = document.createElement('section')
-    const newElementTwo = document.createElement('section')
-    main.appendChild(newElement)
-    newElement.innerHTML = playerDescription
-    main.appendChild(newElementTwo)
-    newElementTwo.innerHTML = playerDescriptionTwo
-})
+const deleteSectionClick = () => {
+    whatIWantToRemove = prompt("Elegí uno de estas opciones: " + arrayOfSections).toLowerCase()
+    console.log(whatIWantToRemove)
+    const myDiv = document.getElementById(whatIWantToRemove)
+    const parent = myDiv.parentNode
+    parent.removeChild(myDiv)
+}
 
 // Agregar jugador
 
